a4b0e52224cd3bd238f1f2dc77dc56fd8d15a23a
unknown <takethisjaguar@gmail.com>
2021-09-30 17:40:07 -0700

    first

---
+++ Leetcode/LinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Leetcode
+{
+    public class LinkedList<T>
+    {
+        public Node<T> head;
+        public LinkedList(List<T> list)
+        {
+            head = new Node<T>(list[0]);
+            var curr = head;
+            for(int i = 1; i < list.Count; i++)
+            {
+                curr.next = new Node<T>(list[i]);
+                curr = curr.next;
+            }
+        }   
+
+        public Node<T> GetHead()
+        {
+            return head;
+        }
+        public void Print(Node<T> head)
+        {
+            while (head != null)
+            {
+                Console.WriteLine(head.value);
+                head = head.next;
+            }
+        }
+    }
+
+    public class Node<T>
+    {
+        public Node<T> next = null;
+        public T value;
+
+        public Node(T value)
+        {
+            this.value = value;
+        }
+    }
+}
---
+++ Leetcode/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine();
+            //SearchRange(new int[] { 5, 7, 7, 8, 8, 10 }, 8);
+            //Console.WriteLine(String.Join(",", SearchRange(new int[] { 5, 7, 7, 8, 8, 10 }, 6)));
+
+            var ll = new LinkedList<int>(new List<int> { 1, 2, 3, 4, 5 });
+            var head1 = ll.head;
+            var head2 = head1;
+
+            var node3 = ll.GetHead();
+            var node4 = node3;
+
+            var node = new Node<int>(5);
+            var node2 = node;
+            node.next = new Node<int>(10);
+
+            head1 = head1.next;
+            node3 = node3.next;
+            //head1 = head1.next
+            Console.WriteLine(head1.value);
+            Console.WriteLine(head2.value);
+        }
+
+        public static Node<int> oddEvenList(Node<int> head)
+        {
+            if (head != null)
+            {
+
+                Node<int> odd = head, even = head.next, evenHead = even;
+
+                while (even != null && even.next != null)
+                {
+                    odd.next = odd.next.next;
+                    even.next = even.next.next;
+                    odd = odd.next;
+                    even = even.next;
+                }
+                odd.next = evenHead;
+            }
+            return head;
+        }
+    }
+}
